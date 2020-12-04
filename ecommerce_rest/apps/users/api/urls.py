3bfb2716576f0b5bfe4b6d7b8db6e019ad7fea55
cesar32az <cesar32az@gmail.com>
2020-12-04 12:37:38 -0600

    implementación de decorador @api_view

--- ecommerce_rest/apps/users/api/urls.py
+++ ecommerce_rest/apps/users/api/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path
-from apps.users.api.api import UserAPIView
+from apps.users.api.api import user_api_view
 
 urlpatterns = [
-    path('api/', UserAPIView.as_view(), name='usuario_api')
+    path('api/', user_api_view, name='usuario_api')
 ]
--- ecommerce_rest/ecommerce_rest/urls.py
+++ ecommerce_rest/ecommerce_rest/urls.py
@@ -18,5 +18,5 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('usuario/', include('apps.users.api.urls')),
+    path('users/', include('apps.users.api.urls')),
 ]
